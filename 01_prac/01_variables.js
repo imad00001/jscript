afd2463a8ab80315c38d6a63cda5b1585e1d1725
imad <153217809+imad00001@users.noreply.github.com>
2025-05-04 10:34:05 +0000

    datatype done

---
+++ 01_prac/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553;//lock
+let  accountEmaail= "farzi@gmail.com";
+var accountpassword = "imad@123";
+accountCity = "jaipur";
+let accountstate
+
+//accountId = 2; // not allowed
+accountEmaail="elioos@123.gmail.com";
+accountpassword = "okemona@123";
+accountCity="india";
+
+console.log(accountId);
+
+/*
+{} (scope)
+prefer not to use var
+block of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmaail,accountpassword,accountCity,accountstate])//(tabular structure)
---
+++ 01_prac/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use script"; // treat all js code as newer version
+
+// alert("hello"); // we are using nodejs, not browser
+
+// code readability is imortant
+
+console.log(3+3);
+
+
+let name = "imad";
+let age = 18;
+let isLoggenIn = false;
+let state ;
+ 
+// number -> 2 to 53 range
+// bigint
+// string ->""
+// boolean -> true/false
+// null -> standalone value
+// undefined ->
+// symbol -> unique
+
+
+// object
+
+console.log(typeof undefined);// o/p -> unndefined
+console.log(typeof null);// o/p -> object 
